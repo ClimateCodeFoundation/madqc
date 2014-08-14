71d052d598001271597672fab1e26ff8c6ee8715
David Jones <drj@pobox.com>
2014-08-14 18:34:26 +0100

    add a bunch of stuff. WIP.

---
+++ README.md
@@ -0,0 +1,10 @@
+Median Absolute Deviation Quality Control
+=========================================
+
+This software will apply a very minimal QC stage to monthly
+temperature data (for example, from GHCN-M or ISTI), using the
+MAD estimator.
+
+Unfinished
+
+    ./mad.py
---
+++ mad.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import itertools
+import os
+
+def station_id(line):
+    return line[:11]
+
+def get_element(line):
+    return line[15:19]
+
+def get_year(line):
+    return int(line[11:15])
+
+class Record(object):
+    def __init__(self, **k):
+        self.__dict__.update(**k)
+
+    def __repr__(self):
+        return "Record({})".format(", ".join(
+          "{}={!r}".format(k,v) for k,v in self.__dict__.items()))
+
+def ghcnm_stations(inp):
+    for id, lines in itertools.groupby(inp, station_id):
+        # Guaranteed stable, is it?
+        by_elem = sorted(lines, key=get_element)
+        for element, lines in itertools.groupby(by_elem, get_element):
+            data = {}
+            record = Record(element=element, id=id, data=data)
+            for line in lines:
+                year = get_year(line)
+                for m in range(12):
+                    v = int(line[19+m*8:24+m*8])
+                    if v != -9999:
+                        v /= 100.0
+                        data["{:04d}{:02d}".format(year, m+1)] = v
+            yield record
+
+
+def main(argv=None):
+    import glob
+
+    pattern = os.path.expanduser("~/.local/share/data/isti")
+    globs = glob.glob(os.path.join(pattern, "merged*.dat"))
+    dat_file = sorted(globs)[-1]
+
+    with open(dat_file) as dat:
+        for record in ghcnm_stations(dat):
+            print(record)
+            break
+
+if __name__ == '__main__':
+    main()
+        
