15f9ec4ffb7e30777be3e7ed56c8d012177c6db4
David Jones <drj@pobox.com>
2014-08-14 19:30:49 +0100

    test that element returned by median is in list

--- test_mad.py
+++ test_mad.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
+import random
+
 import mad
 
 def test_1():
     assert 2 == mad.median([1,2,3])
+
+def test_2():
+    m = random.choice(range(1,10))
+    e = random.choice(range(4))
+    n = m*10**e
+    
+    l = list(range(n))
+    random.shuffle(l)
+
+    assert mad.median(l) in l
 
 def main():
     for name, fn in globals().items():
